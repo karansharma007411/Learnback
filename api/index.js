bf73cc9cfd5fcf01d8bfc38a3af09a56ae3c37ad
Karan <ks3979280@gmail.com>
2025-05-15 09:33:52 +0530

    made serverless

--- api/index.js
+++ api/index.js
@@ -2,19 +2,19 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import connectDB from "./database/db.js";
-import userRoute from "./routes/user.route.js";
-import courseRoute from "./routes/course.route.js";
-import mediaRoute from "./routes/media.route.js";
-import purchaseRoute from "./routes/purchaseCourse.route.js";
-import courseProgressRoute from "./routes/courseProgress.route.js";
-
+import connectDB from "../database/db.js";
+import userRoute from "../routes/user.route.js";
+import courseRoute from "../routes/course.route.js";
+import mediaRoute from "../routes/media.route.js";
+import purchaseRoute from "../routes/purchaseCourse.route.js";
+import courseProgressRoute from "../routes/courseProgress.route.js";
+import ServerlessHttp from "serverless-http";
 dotenv.config({});
 
 // call database connection here
 connectDB();
 const app = express();
-
+const serverless = ServerlessHttp;
 const PORT = process.env.PORT || 3000;
 
 // default middleware
@@ -34,8 +34,4 @@ app.use("/api/v1/purchase", purchaseRoute);
 app.use("/api/v1/progress", courseProgressRoute);
  
  
-app.listen(PORT, () => {
-    console.log(`Server listen at port ${PORT}`);
-})
-
-
+module.exports.handler = serverless(app);
